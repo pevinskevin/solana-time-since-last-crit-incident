3be430c5e5517ee543dd5f97431561a0e6b5287d
Kevin Strandberg <kevin@npstrandberg.comm>
2025-03-09 15:18:38 +0100

    Trying out different directory structures etc.

--- app.js
+++ app.js
@@ -2,12 +2,12 @@ import express from 'express';
 const app = express();
 import path from 'path';
 
-// app.use(express.static('public'));
+app.use(express.static('public'));
 
-import { funcMsSinceLastCritIncident } from './util/timeConverter.js';
 
 app.get('/', async (req, res) => {
-	const obj = fetchIncident();
+	res.sendFile(path.resolve('./public/frontpage/index.html'))
+	const obj = await fetchIncident();
 	res.send({ data: obj });
 });
 
@@ -15,24 +15,4 @@ app.listen(8080, () => {
 	console.log('Server is running.');
 });
 
-const fetchIncident = async function () {
-	const promise = await fetch(
-		'https://status.solana.com/api/v2/incidents.json'
-	);
-	const data = await promise.json();
-	const incidents = data.incidents;
-	const criticalIncidentsArray = [];
-	filterCriticalIncidents(incidents);
 
-	// Filter all objects with 'impact: "critical"' to new array.
-	function filterCriticalIncidents(incidents) {
-		criticalIncidentsArray.push(
-			...incidents.filter((incident) => incident.impact === 'critical')
-		);
-	}
-
-	const timeSinceLastCritIncident = funcMsSinceLastCritIncident(
-		criticalIncidentsArray[0].resolved_at
-	);
-	return timeSinceLastCritIncident;
-};
